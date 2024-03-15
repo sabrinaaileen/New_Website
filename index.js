753e670dbc9a6671cbb2b22df3df048f25688d8a
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2024-03-15 13:45:25 +0100

    Added a photo section and started with event-info section

---
+++ index.js
@@ -0,0 +1,7 @@
+let eventUpcoming = document.getElementById("event-text-upcoming");
+
+function showUpcomingText() {
+  eventUpcoming.innerHTML =
+    "<ul><li>14. April 2024: Die THRILLERPFEIFEN - Mord im E-Bike-Laden in der Pasinger Fabrik München</li></ul>";
+}
+showUpcomingText();
